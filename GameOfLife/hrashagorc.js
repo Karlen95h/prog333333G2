1551d9b719a9ffd361bf3d127b7031b48f89ec3c
Karlen95 <karlen.h.1995@gmail.com>
2023-12-01 19:17:23 +0400

    inhertance

---
+++ GameOfLife/hrashagorc.js
@@ -0,0 +1,51 @@
+class hrashagorc extends LivingCreature {
+    constructor(x, y) {
+       super(x,y)
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    getNewCordinates() {
+        this.directions = [
+
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(char) {
+        this.getNewCordinates();
+        return super.chooseCell(char)
+
+    }
+
+    move() {
+        let found = this.chooseCell(0);
+        let exact = random(found)
+
+        if (exact) {
+            let x = exact[0];
+            let y = exact[1];
+            matrix[y][x] = 4;
+            let grasseater = new GrassEater(this.x, this.y);
+            matrix[this.y][this.x] = 2;
+            grassEaterArr.push(grasseater);
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+}
+
